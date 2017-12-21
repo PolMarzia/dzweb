163179ae6a6d18704b6e005095f7a4b279523f37
PolMarzia <asya1997@mail.ru>
2017-12-21 18:36:22 +0300

    Создана одна база данных

--- mainsite/urls.py
+++ mainsite/urls.py
@@ -1,6 +1,8 @@
 from mainsite.views import *
 from django.conf.urls import url
 from mainsite import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^$', views.mainpage, name='index'),
@@ -8,4 +10,4 @@ urlpatterns = [
     url(r'^skazki_babushki_larisy/', views.skazki, name='skazki_lar'),
     url(r'^skazki_unih_darovanii/', views.skazki_dar, name='skazki_dar'),
     url(r'^risunki/', views.risunki, name='risunki')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
